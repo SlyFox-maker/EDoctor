2cd6de7fa75fe195d55df916fc61ce5a6403691d
SlyFox-maker <skyfox4995@gmail.com>
2024-05-26 12:31:41 -0600

    Flexible navigation menu for not spam functions in the code

--- EDoctor/MainWindow.xaml.cs
+++ EDoctor/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,42 +21,49 @@ namespace EDoctor
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Dictionary<String, StackPanel> keyValueNavigation = new Dictionary<String, StackPanel>();
         public MainWindow(String login)
         {
             InitializeComponent();
+
+            //Registration of buttons from navigation panel
+
+            foreach(var child in navigatorPanel.Children)
+            {
+                if (child is Button button)
+                {
+                    button.Click += navigationInPanelClick;
+                    
+                }
+            }
+
+            keyValueNavigation.Add("tagButtonPacientes",tabPacientes);
+            keyValueNavigation.Add("tagButtonAgenda",tabAgenda);
+            keyValueNavigation.Add("tagButtonsNotas", tabNotas);
+            keyValueNavigation.Add("tagButtonsCitas", tabCitas);
+            keyValueNavigation.Add("tagButtonsSolicitudDelLabarotorio",null);
+            keyValueNavigation.Add("tagButtonSolicitudDelRX", null);
+            keyValueNavigation.Add("tagButtonsMDA", null);
+            keyValueNavigation.Add("tagButtonsCertificadoMedico", null);
+            keyValueNavigation.Add("tagButtonsFormatos", null);
+            keyValueNavigation.Add("tagButtonsReferencias", null);
+
         }
 
-
-        private void tagButtonPacientes_Click(object sender, RoutedEventArgs e)
+        private void navigationInPanelClick(object sender, RoutedEventArgs e)
         {
             clearTagNavigation();
 
-            tagButtonPacientes.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#5ba2f4"));
-            tabPacientes.Visibility = Visibility.Visible;
-        }
+            if (sender is Button clickedButton)
+            {
 
-        private void tagButtonAgenda_Click(object sender, RoutedEventArgs e)
-        {
-            clearTagNavigation();
-
-            tagButtonAgenda.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#5ba2f4"));
-            tabAgenda.Visibility = Visibility.Visible;
-        }
-
-        private void tagButtonsNotas_Click(object sender, RoutedEventArgs e)
-        {
-            clearTagNavigation();
-
-            tagButtonsNotas.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#5ba2f4"));
-            tabNotas.Visibility = Visibility.Visible;
-        }
-
-        private void tagButtonsCitas_Click(object sender, RoutedEventArgs e)
-        {
-            clearTagNavigation();
-
-            tagButtonsCitas.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#5ba2f4"));
-            tabCitas.Visibility = Visibility.Visible;
+                var panel = keyValueNavigation[clickedButton.Name];
+                if(panel != null)
+                {
+                    clickedButton.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#5ba2f4"));
+                    panel.Visibility = Visibility.Visible;
+                }
+            }
         }
 
 
